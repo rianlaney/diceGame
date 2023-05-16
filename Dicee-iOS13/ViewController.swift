0f737b10860f435c6755c0150fe09e88afb54079
rianlaney <rianlaney@gmail.com>
2023-05-16 12:50:16 -0400

    test

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
 
     @IBAction func rollBtn(_ sender: Any) {
         let diceArr = [UIImage(named: "DiceSix"), UIImage(named: "DiceFive"), UIImage(named: "DiceFour"), UIImage(named: "DiceThree"), UIImage(named: "DiceTwo"), UIImage(named: "DiceOne")]
+        //
         
 //        diceImageViewLeft.image = diceArr.randomElement(); //quicker less heavy of code
         diceImageViewLeft.image =  diceArr [Int.random(in: 0...5)]
